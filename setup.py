6753fb209fcfba91267092d734f4fdc82a917615
wr1 <8971152+wr1@users.noreply.github.com>
2022-01-19 23:23:55 +0100

    add b3p prefix to command line tools

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ setuptools.setup(
     packages=["b3p"],
     entry_points={
         "console_scripts": [
-            "build_blade_geometry=b3p.build_blade_geometry:main",
-            "build_blade_structure=b3p.build_blade_structure:main",
-            "build_plybook=b3p.build_plybook:main",
-            "drape_mesh=b3p.drape_mesh:main",
-            "combine_meshes=b3p.combine_meshes:main",
-            "mesh_2d=b3p.mesh_2d:main",
+            "b3p_blade_geometry=b3p.build_blade_geometry:main",
+            "b3p_blade_structure=b3p.build_blade_structure:main",
+            "b3p_plybook=b3p.build_plybook:main",
+            "b3p_drape=b3p.drape_mesh:main",
+            "b3p_meshcombine=b3p.combine_meshes:main",
+            "b3p_2dmesh=b3p.mesh_2d:main",
+            "b3p_add_load=b3p.add_load_to_mesh:main",
+            "b3p_export_ccx=b3p.mesh2ccx:main",
+            "b3p_frd2vtu=b3p.frd2vtu:main",
         ],
     },
 )
