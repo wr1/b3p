7ea2f6d5f563550909a69bb58bef60dad47891c6
wr1 <8971152+wr1@users.noreply.github.com>
2022-01-22 22:39:10 +0100

    add anba solve and prep (only works in fenics docker)

---
+++ b3p/anba4_prep.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+import pyvista as pv
+import argparse
+import multiprocessing
+
+
+def conv3d_2d(mesh):
+    "hack to translate a mesh to 2D format, which is what fenics/anba needs"
+    x = open(mesh, "r").read()
+    nb = x[: x.find("Topology")]
+    rest = x[x.find("Topology") :]
+    nb = nb.replace("0.0000000e+00\n", "\n")
+    nb = nb.replace(' 3"', ' 2"')
+    nb = nb.replace("XYZ", "XY")
+    open(mesh, "w").write(nb + rest)
+
+
+def vtp2xdmf(vtp):
+    assert vtp.endswith(".vtp")
+    mesh = pv.read(vtp)
+    tri = mesh.triangulate()  # fenics doesn't do mixed element types
+    tri.points[:, 2] = 0
+    xd = vtp.replace(".vtp", ".xdmf")
+    pv.save_meshio(xd, tri, data_format="XML")
+    conv3d_2d(xd)
+    print("converted %s to %s" % (vtp, xd))
+
+
+def main():
+    p = argparse.ArgumentParser(
+        description="translate section meshes from vtk to XDMF and 2D"
+    )
+    p.add_argument("sections", nargs="*", help="section meshes in .vtp format")
+    args = p.parse_args()
+    p = multiprocessing.Pool()
+    p.map(vtp2xdmf, args.sections)
+    p.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ b3p/anba4_solve.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+from dolfin import *
+from anba4 import *
+import argparse
+import multiprocessing
+import json
+
+
+def run_mesh(meshname):
+    mesh = Mesh()
+    with XDMFFile(meshname) as infile:
+        infile.read(mesh)
+
+    # Basic material parameters. 9 is needed for orthotropic materials. TODO materials
+    E = 1.0
+    nu = 0.33
+    # Assmble into material mechanical property Matrix.
+
+    matMechanicProp = [E, nu]
+    # Meshing domain.
+    # CompiledSubDomain
+    materials = MeshFunction("size_t", mesh, mesh.topology().dim())
+    fiber_orientations = MeshFunction("double", mesh, mesh.topology().dim())
+    plane_orientations = MeshFunction("double", mesh, mesh.topology().dim())
+
+    materials.set_all(0)
+    fiber_orientations.set_all(0.0)
+    plane_orientations.set_all(90.0)
+
+    # Build material property library.
+    mat1 = material.IsotropicMaterial(matMechanicProp, 1.0)
+
+    matLibrary = []
+    matLibrary.append(mat1)
+
+    anba = anbax(mesh, 2, matLibrary, materials, plane_orientations, fiber_orientations)
+    stiff = anba.compute()
+
+    stiffness_matrix = stiff.getDenseArray()
+
+    mass = anba.inertia()
+
+    mass_matrix = mass.getDenseArray()
+
+    decoupled_stiff = DecoupleStiffness(stiff)
+
+    angle = PrincipalAxesRotationAngle(decoupled_stiff)
+
+    mass_center = ComputeMassCenter(mass)
+    tension_center = ComputeTensionCenter(stiffness_matrix)
+    shear_center = ComputeShearCenter(stiffness_matrix)
+    output = {
+        "name": meshname,
+        "stiffness": stiffness_matrix.tolist(),
+        "mass_matrix": mass_matrix.tolist(),
+        "decoupled_stiffness": decoupled_stiff.tolist(),
+        "principal_axes_rotation": angle,
+        "mass_center": mass_center,
+        "tension_center": tension_center,
+        "shear_center": shear_center,
+    }
+
+    with open(meshname + ".json", "w") as write_file:
+        json.dump(output, write_file, indent=4)
+
+
+def main():
+    p = argparse.ArgumentParser(description="run a series of sections through anba4")
+    p.add_argument("meshes", nargs="*")
+
+    args = p.parse_args()
+
+    p = multiprocessing.Pool()
+    r = p.map_async(
+        run_mesh, args.meshes
+    )  # run async seems to avoid a sporadic blocking error
+    r.wait()
+
+
+if __name__ == "__main__":
+    main()
