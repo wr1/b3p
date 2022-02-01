771476ab8ae3fbf14593231e84b92f5c6f79ef94
wr1 <8971152+wr1@users.noreply.github.com>
2022-02-01 21:44:22 +0000

    anba, adding materials, work in progress

--- b3p/anba4_solve.py
+++ b3p/anba4_solve.py
@@ -4,18 +4,77 @@ from anba4 import *
 import argparse
 import multiprocessing
 import json
+import numpy as np
+import yaml
+import os 
+import pyvista as pv 
+
+def get_material_db(material_map):
+    assert os.path.isfile(material_map)
+    mm = json.load(open(material_map,'r'))
+    gdir = os.path.dirname(material_map)
+    
+    mat_db = None
+    if "matdb" in mm:  # check if the material map file points to a material db
+        mat_db = yaml.load(
+            open(os.path.join(gdir, mm["matdb"])), Loader=yaml.CLoader
+        )
+    else:
+        exit(
+            "material map available, but no link to material db, need matdb definition to do FEA"
+        )
+
+
+    mm_inv = {v: k for k, v in mm.items()}
+
+    for i in mm_inv:
+        if i !=mm['matdb']:      
+            material = mat_db[mm_inv[i]]
+            if 'vf' in material: # ortho material
+                matMechanicProp = np.zeros((3,3))
+                matMechanicProp[0,0] = material['tEx']*1e6 # e_xx
+                matMechanicProp[0,1] = material['tEy']*1e6 # e_yy
+                matMechanicProp[0,2] = material['tEz']*1e6 # e_zz
+                matMechanicProp[1,0] = material['tGyz']*1e6#g_yz
+                matMechanicProp[1,1] = material['tGxz']*1e6#g_xz
+                matMechanicProp[1,2] = material['tGxy']*1e6#g_xy
+                matMechanicProp[2,0] = material['tnuyz']#nu_zy
+                matMechanicProp[2,1] = material['tnuxz']#nu_zx
+                matMechanicProp[2,2] = material['tnuxy']#nu_xy
+            else:
+                
+            print(i)
+
+
+
+
+
+
+    return ( mm_inv, mat_db )
+
+
+
 
 
 def run_mesh(meshname):
+
+    infile = XDMFFile(meshname)
     mesh = Mesh()
-    with XDMFFile(meshname) as infile:
-        infile.read(mesh)
+    infile.read(mesh)
+
+    pvmesh = pv.read(meshname)
+
+
+    matid = MeshFunction("size_t", mesh, mesh.topology().dim())
+
+
 
     # Basic material parameters. 9 is needed for orthotropic materials.
     # TODO materials and orientations
     E = 1.0
     nu = 0.33
     # Assmble into material mechanical property Matrix.
+
 
     matMechanicProp = [E, nu]
     # Meshing domain.
@@ -24,7 +83,15 @@ def run_mesh(meshname):
     fiber_orientations = MeshFunction("double", mesh, mesh.topology().dim())
     plane_orientations = MeshFunction("double", mesh, mesh.topology().dim())
 
-    materials.set_all(0)
+    print(dir(materials))
+
+    matuniq = np.unique(pvmesh.cell_data['mat'])
+
+
+    # map for materials in this section 
+    mat_map_0 = dict(zip(matuniq,range(len(matuniq)))) 
+
+    materials.set_values([mat_map_0[i] for i in pvmesh.cell_data['mat'].tolist()])
     fiber_orientations.set_all(0.0)
     plane_orientations.set_all(90.0)
 
@@ -68,14 +135,21 @@ def run_mesh(meshname):
 def main():
     p = argparse.ArgumentParser(description="run a series of sections through anba4")
     p.add_argument("meshes", nargs="*")
-
+    p.add_argument("matdb",help="material map json file")
+    p.add_argument('--debug',action='store_true')
     args = p.parse_args()
 
-    p = multiprocessing.Pool()
-    r = p.map_async(
-        run_mesh, args.meshes
-    )  # run async seems to avoid a sporadic blocking error
-    r.wait()
+    mdb = get_material_db(args.matdb)
+    exit()
+    if args.debug:
+        for i in args.meshes:
+            run_mesh(i)
+    else:
+        p = multiprocessing.Pool()
+        r = p.map_async(
+            run_mesh, args.meshes
+        )  # run async seems to avoid a sporadic blocking error
+        r.wait()
 
 
 if __name__ == "__main__":
