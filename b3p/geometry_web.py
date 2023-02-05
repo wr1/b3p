98332aefb200523bfb4e6f4b0fc7b90f3b8ebdbb
wr1 <8971152+wr1@users.noreply.github.com>
2023-02-05 14:22:18 +0100

    vectorize mesh generation and translate to pyvista

--- b3p/geometry_web.py
+++ b3p/geometry_web.py
@@ -1,6 +1,7 @@
 import vtk
-import numpy
+import numpy as np
 from b3p import geom_utils
+import pyvista as pv
 
 
 def equals(v1, v2):
@@ -8,35 +9,36 @@ def equals(v1, v2):
     return (v1 - v2) ** 2 < tol
 
 
-def mesh_line(pnt1, pnt2, np, id):
+def mesh_line(pnt1, pnt2, n_cells, id):
     """
     utility to mesh a line, adds a couple of parametric coordinates to aid
     draping
     """
     xyz = []
-    tol = 1e-6
+    # tol = 1e-6
     web_height = vtk.vtkGeoMath().DistanceSquared(pnt1, pnt2) ** 0.5
 
     for i in zip(pnt1, pnt2):
         mm = min(0.3, 0.06 / web_height)
-        rel = sorted([0, 1] + list(numpy.linspace(mm, 1.0 - mm, np - 2)))
+        rel = sorted([0, 1] + list(np.linspace(mm, 1.0 - mm, n_cells - 2)))
         ab = [j * (i[1] - i[0]) + i[0] for j in rel]
-        xyz.append(numpy.array(ab))
+        xyz.append(np.array(ab))
 
     dst = [i[1:] - i[:-1] for i in xyz]  # distances between points in 3 dimensions
     sl = (dst[0] ** 2 + dst[1] ** 2 + dst[2] ** 2) ** 0.5  # length of the line segments
 
-    pl = [0] + [
-        sum(sl[:i]) for i in range(1, len(sl) + 1)
-    ]  # path location from the first web point
+    # path location from the first web point
+    pl = [0] + [sum(sl[:i]) for i in range(1, len(sl) + 1)]
 
     ppl = [-i + pl[-1] for i in pl]
     ml = [abs(i - 0.5 * web_height) for i in pl]  # distance from the web centerline
 
+    # print(len(ml), len(pl))
+
     wh = [web_height for _ in ml]
 
-    rad = numpy.mean(xyz[2])
-    r = [rad for _ in range(np)]
+    rad = np.mean(xyz[2])
+    r = [rad for _ in range(n_cells)]
 
     arrays = {
         "d_te": pl,
@@ -44,7 +46,7 @@ def mesh_line(pnt1, pnt2, np, id):
         "d_le_r": [i / max(ppl) for i in ppl],
         f"d_{id}_r": [i / max(ml) for i in ml],
         f"d_{id}": ml,
-        "d_along_airfoil": ml,
+        # "d_along_airfoil": ml,
         "web_height": wh,
         "radius": r,
         "is_web": [1 for _ in ppl],
@@ -80,7 +82,7 @@ class web:
         strips)
         """
         g = list(zip(*self.points))
-        return numpy.mean(g[1]), numpy.mean(g[2])
+        return np.mean(g[1]), np.mean(g[2])
 
     def splits(self, r, r_relative):
         """
@@ -122,58 +124,59 @@ class web:
                     self.evaluations[rmm].append(pnt)
 
     def _create_quad_connectivity(self, n_points, n_total):
+        nrows = int(n_total / n_points)
+        colids = np.arange(n_points - 1)
+        npp = (
+            np.arange(1, nrows).repeat(n_points).reshape(nrows - 1, n_points) - 1
+        ) * n_points + colids
+        stck = [np.ones_like(npp) * 4, npp, npp + n_points, npp + n_points + 1, npp + 1]
+
+        print(self.flip_normal)
+        return np.hstack(list(reversed(stck)) if self.flip_normal else stck).flatten()
+
+    def _create_quad_connectivity(self, n_points, n_total):
         """
         create the connectivity of the points that make up the mesh
         """
-        quads = vtk.vtkCellArray()
+        connectivity = []
         for i in range(1, int(n_total / n_points)):
-            np = range((i - 1) * n_points, i * n_points)  # previous row point ids
-            nc = range(i * n_points, (i + 1) * n_points)  # current row point ids
+            npp = np.arange((i - 1) * n_points, i * n_points)
+            ncc = np.arange(i * n_points, (i + 1) * n_points)
             for j in range(n_points - 1):
-                quads.InsertNextCell(4)
+                elem = [
+                    npp[j],
+                    ncc[j],
+                    ncc[(j + 1) % n_points],
+                    npp[(j + 1) % n_points],
+                ]
                 if not self.flip_normal:
-                    quads.InsertCellPoint(np[j])
-                    quads.InsertCellPoint(nc[j])
-                    quads.InsertCellPoint(nc[(j + 1) % n_points])
-                    quads.InsertCellPoint(np[(j + 1) % n_points])
+                    connectivity.append([4] + elem)
                 else:
-                    quads.InsertCellPoint(np[(j + 1) % n_points])
-                    quads.InsertCellPoint(nc[(j + 1) % n_points])
-                    quads.InsertCellPoint(nc[j])
-                    quads.InsertCellPoint(np[j])
+                    connectivity.append([4] + list(reversed(elem)))
 
-        self.mesh.SetPolys(quads)
+        return np.array(connectivity).flatten()
 
     def _create_points(self, n_cells):
         """
         generate the points needed to build the mesh
         """
         ev = self.evaluations
-        vp = vtk.vtkPoints()
-        mesh = vtk.vtkPolyData()
+        vp = []
         added_arrays = {}
         for i in sorted(ev):
             if len(ev[i]) == 3:
                 pnts, data = mesh_line(ev[i][1], ev[i][2], n_cells, self.coordinate)
-                for pnt in pnts:
-                    vp.InsertNextPoint(pnt)
+                vp.extend(pnts)
                 for j in data:
                     if j not in added_arrays:
-                        added_arrays[j] = vtk.vtkFloatArray()
-                        added_arrays[j].SetName(j)
-                    for k in data[j]:
-                        added_arrays[j].InsertNextValue(k)
+                        added_arrays[j] = data[j]
+                    else:
+                        added_arrays[j].extend(data[j])
 
-        mesh.SetPoints(vp)
-        for value in added_arrays.values():
-            mesh.GetPointData().AddArray(value)
-        self.mesh = mesh
+        return vp, added_arrays
 
     def write_mesh(self, vtpfile):
-        writer = vtk.vtkXMLPolyDataWriter()
-        writer.SetFileName(vtpfile)
-        writer.SetInputData(self.mesh)
-        writer.Write()
+        self.mesh.save(vtpfile)
         print(f"# wrote mesh to {vtpfile}")
 
     def mesh(self, mesh, n_cells=20):
@@ -181,5 +184,9 @@ class web:
         main interface for meshing a web
         """
         self._find_top_and_bottom_points(mesh)
-        self._create_points(n_cells)
-        self._create_quad_connectivity(n_cells, self.mesh.GetNumberOfPoints())
+        points, pdata = self._create_points(n_cells)
+        cells = self._create_quad_connectivity(n_cells, len(points))
+
+        self.mesh = pv.PolyData(points, faces=cells)
+        for i in pdata:
+            self.mesh.point_data[i] = pdata[i]
