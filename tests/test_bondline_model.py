c43c542c180c68868dde5443451d29916c707e76
wr1 <8971152+wr1@users.noreply.github.com>
2025-04-07 12:08:45 +0200

    fix test for bondline model

--- tests/test_bondline_model.py
+++ tests/test_bondline_model.py
@@ -1,12 +1,11 @@
-from .test_build import run_build
-
 import pyvista as pv
+from .test_build import run_build
 
 
 def test_bondline_model(run_build):
     """Test if the bondline model is created."""
-    bondline_vtu = run_build["workdir"].glob("*bondline.vtu")
-    print(bondline_vtu)
-
-    # assert True
-    # assert build_output_exists()
+    workdir = run_build["workdir"]
+    bondline_vtu = list(workdir.glob("*bondline.vtu"))
+    assert bondline_vtu, "Bondline VTU file should exist"
+    vtu = pv.read(bondline_vtu[0])
+    assert vtu.n_points > 0, "Bondline VTU should contain points"
